4213abf98a4afca78aab06ad0ff6b4c0a8bded2e
Zee <omarzee@aucegypt.edu>
2024-07-18 16:30:35 +0300

    Project done

--- cache.cpp
+++ cache.cpp
@@ -91,11 +91,11 @@ void calculateAndPrintBits(unsigned int cacheLineSize) {
     indexSize = log2(numberOfLines);
     tagSize = 32 - indexSize - offsetSize;
 
-    cout << "Cache Line Size: " << cacheLineSize << " bytes" << endl;
-    cout << "Offset bits: " << offsetSize << endl;
-    cout << "Index bits: " << indexSize << endl;
-    cout << "Tag bits: " << tagSize << endl;
-    cout << "---------------------------------" << endl;
+    // cout << "Cache Line Size: " << cacheLineSize << " bytes" << endl;
+    // cout << "Offset bits: " << offsetSize << endl;
+    // cout << "Index bits: " << indexSize << endl;
+    // cout << "Tag bits: " << tagSize << endl;
+    // cout << "---------------------------------" << endl;
 }
 
 unsigned int getOffset(unsigned int addr) 
@@ -132,11 +132,11 @@ cacheResType cacheSimDM(unsigned int addr, unsigned int lineSize)
 	}
 
 	// Print the address, offset, index, tag, and valid bit
-	cout << "Address = " << address << endl;
-	cout << "Offset = " << unsignedIntToBinaryString(offset) << endl;
-	cout << "Index = " << unsignedIntToBinaryString(index) << endl;
-	cout << "Tag = " << unsignedIntToBinaryString(tag) << endl;
-	cout << "Valid = " << valid << endl;
+	// cout << "Address = " << address << endl;
+	// cout << "Offset = " << unsignedIntToBinaryString(offset) << endl;
+	// cout << "Index = " << unsignedIntToBinaryString(index) << endl;
+	// cout << "Tag = " << unsignedIntToBinaryString(tag) << endl;
+	// cout << "Valid = " << valid << endl;
 
 	cache.resize(numberOfLines, vector<int>(lineSize / 4, -1)); // Initialize the cache with -1
 
@@ -163,13 +163,53 @@ cacheResType cacheSimFA(unsigned int addr, unsigned int lineSize)
 	// This function accepts the memory address for the read and 
 	// returns whether it caused a cache miss or a cache hit
 
+	calculateAndPrintBits(lineSize);
+	// This function accepts the memory address for the memory transaction and 
+	// returns whether it caused a cache miss or a cache hit
+	// Dividing the address into three parts: offset, index, and tag
+
+	tag = addr;
+
+	cache.resize(numberOfLines, vector<int>(lineSize / 4, -1)); // Initialize the cache with -1
+
+	int counter = 0;
+
+	for(int i = 0; i < numberOfLines; i++) {
+		for(int j = 0; j < lineSize/4 + 1; j++) {
+			if(cache[i][j] == tag) {
+				return HIT;
+			}
+			else if(cache[i][j] == -1) {
+				cache[i][j] = tag;
+				return MISS;
+			}
+			if(counter == numberOfLines - 1){
+				int x = rand() % numberOfLines; // Randomly select an index
+				cache[x][j] = tag; // Update the tag
+			}
+		}
+		counter++;
+	}
+	// if(cache[index][offset/4] == -1) { // If the cache is empty
+	// 	cache[index][offset/4] = tag; // Store the tag
+	// 	return MISS; // Return miss(cold start miss)
+	// }
+	// else if(cache[index][offset/4] == tag) { // If the cache is not empty and the tag matches
+	// 	return HIT; // Return hit
+	// }
+	// else { // If the cache is at full capacity and the tag does not match
+	// 	index = rand() % numberOfLines; // Randomly select an index
+	// 	cache[index][offset/4] = tag; // Update the tag
+	// 	return MISS; // Return miss(capacity miss)
+	// }
+
 	// The current implementation assumes there is no cache; so, every transaction is a miss
 	return MISS;
 }
 
 char *msg[2] = {"Miss","Hit"};
 
-#define NO_OF_Iterations 10000 // Change to 1,000,000
+#define NO_OF_Iterations 100000 // Change to 1,000,000
 
 int main()
 {
@@ -192,16 +232,18 @@ int main()
 	}
 	cout << "Hit ratio = " << (100*hit/NO_OF_Iterations)<< "%" << endl;
 
+	cout << "-----------------------------------------------------------------------------------------------" << endl;
+
 	cout << "Fully Associative Cache Simulator\n";
 
-	// for(int inst=0;inst<NO_OF_Iterations;inst++)
-	// {
-	// 	addr = memGen2();
-	// 	r = cacheSimFA(addr, 16);
-	// 	if(r == HIT) hit++;
-	// 	cout <<"0x" << setfill('0') << setw(8) << hex << addr <<" ("<< msg[r] <<")\n";
-	// }
-	// cout << "Hit ratio = " << (100*hit/NO_OF_Iterations)<< "%" << endl;
+	for(int inst=0;inst<NO_OF_Iterations;inst++)
+	{
+		addr = memGen2();
+		r = cacheSimFA(addr, lineSize);
+		if(r == HIT) hit++;
+		cout <<"0x" << setfill('0') << setw(8) << hex << addr <<" ("<< msg[r] <<")\n";
+	}
+	cout << "Hit ratio = " << (100*hit/NO_OF_Iterations)<< "%" << endl;
 
 	return 0;
 }
